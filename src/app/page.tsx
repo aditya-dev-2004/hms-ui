4a45dc7a6ef23d0707e5cbd147a8cd3b8ce22d2d
aditya-dev-2004 <dev.adityakumar92@gmail.com>
2025-03-24 07:43:31 +0530

    24-03-25\

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import AwardSection from "@/Components/Landing Page/AwardSection"
+import Booking from "@/Components/Landing Page/Booking"
+import Carousel from "@/Components/Landing Page/Carousel"
+import Cout from "@/Components/Landing Page/Cout"
+import FeedbackCarousel from "@/Components/Landing Page/FeedbackCarousel"
+
+import Partners from "@/Components/Landing Page/Partners"
+import PersonalCare from "@/Components/Landing Page/PersonalCare"
+import Services from "@/Components/Landing Page/Services"
+
+export default function Home() {
+  return(
+    <>
+   
+    <Carousel/>
+    <PersonalCare/>
+    <Cout/>
+    <AwardSection/>
+    <Services/>
+    <Booking/>
+    <FeedbackCarousel/>
+    <Partners/>
+    
+   
+    </>
+  )
+}
