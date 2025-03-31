999b3bcf7a4842ab6d206e3f3830759a21327388
aditya-dev-2004 <dev.adityakumar92@gmail.com>
2025-03-31 09:19:56 +0530

    new

--- src/Components/Common/UserRegister.tsx
+++ src/Components/Common/UserRegister.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import * as yup from "yup";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
+import { userAuthRegister } from '@/Services';
+import { swalFire } from '@/Helpers/SwalFire';
 
 const doctorSchema = yup.object().shape({
   name: yup.string().min(2).max(50).required("Name is required"),
@@ -26,34 +28,60 @@ const patientSchema = yup.object().shape({
   gender: yup.string().oneOf(["Male", "Female", "Other"], "Invalid gender").required(),
   contact: yup.string().matches(/\d{10}/, "Contact must be a 10-digit number").required(),
   age: yup.number().typeError("Age must be a number").min(18).max(100).required(),
+  profile: yup.mixed().test("fileSize", "File is required", (value:any) => value?.length > 0).required(),
   userType: yup.string().oneOf(["Doctor", "Patient"], "Invalid User Type").required(),
 });
 
 const UserRegister = () => {
-  const [userType, setUserType] = useState("Patient");
-  const isDoctor = userType === "Doctor";
+  const [userType, setUserType] = useState("patient");
+  const isDoctor = userType === "doctor";
   
   const schema:any = isDoctor ? doctorSchema : patientSchema;
   const { register, handleSubmit, formState: { errors } }:any = useForm({
     resolver: yupResolver(schema),
   });
 
+   const registerFunction=async(data:any)=>{
+    const formData :any =new FormData()
+    formData.append("name",data.name)
+    formData.append("email",data.email)
+    formData.append("contact",data.contact)
+    formData.append("profile",data.profile)
+    formData.append("gender",data.gender)
+    formData.append("age",data.age)
+    formData.append("userType",data.userType)
+    formData.append("departmentId",data.departmentId)
+    formData.append("specialist",data.specialist)
+    formData.append("qualifications",data.qualifications)
+    formData.append("experience",data.experience)
+    formData.append("fees",data.fees)
+    formData.append("address",data.address)
+    formData.append("experience",data.experience)
+
+      const res=await userAuthRegister(formData)
+      
+      if(res?.code==200){
+        swalFire("Auth",res.message,"success") 
+      }else{  
+        swalFire("Auth",res.message,"error")
+      } 
+    }
   return (
     <>
      <div className="row login-row px-3 my-bg-color1">
         <div className="col-md-4 mx-auto my-bg-color2 text-light my-5 rounded-3 p-5">
           <h2 className='my-color3 mb-4 fw-bold text-center'>Register</h2>
-          <form onSubmit={handleSubmit((d:any)=>console.log(d))}>
+          <form onSubmit={handleSubmit((d:any)=>registerFunction(d))}>
          <div className="mb-4">
          <label className="me-2 fw-bold">Register as:</label>
         <select {...register("userType")} className="form-select rounded-0 text-light myform-select d-inline-block mt-1 mb-3" value={userType} onChange={(e)=>setUserType(e.target.value)}>
-          <option className='t' value="Doctor">Doctor</option>
-          <option className='t' value="Patient">Patient</option>
+          <option className='t' value="doctor">Doctor</option>
+          <option className='t' value="patient">Patient</option>
         </select>
         {errors.userType && <div className="text-danger fw-bold">{errors.userType?.message}</div>}
          </div>
 
-        {userType==="Patient" && (
+        {userType==="patient" && (
           <>
           <div className='mb-4'>
               <input {...register("name")} className='myform-control form-control ps-0 text-light rounded-0 mt-1' placeholder='Enter your name' type="text" id="name" />
@@ -84,12 +112,16 @@ const UserRegister = () => {
               <input {...register("age")} className='myform-control form-control ps-0 text-light rounded-0 mt-1' placeholder='Enter your age' type="number" id="age" />
               {errors.age && <div className="text-danger fw-bold">{errors.age?.message}</div>}
             </div>
+            <div className='mb-4'>
+            <input {...register("profile")} className='myform-control form-control ps-0 text-light rounded-0 mt-1' type="file" id="profile" />
+              {errors.profile && <div className="text-danger fw-bold">{errors.profile?.message}</div>}
+            </div>
 
             <input type="submit" value="Register" className='w-100 mx-auto d-block text-light my-btn-hover1 btn mt-4 my-bg-color1' />
           </>
         )}
 
-        {userType==="Doctor" && (
+        {userType==="doctor" && (
           <>
            <div className="row">
             <div className='col-md-6 mb-4'>
--- src/Services/index.ts
+++ src/Services/index.ts
@@ -5,4 +5,8 @@ export const userAuthLogin=async(payload:any)=>{
     const response=await axios.post(`http://localhost:9000/v1/api/login`,payload);
     return response?.data
    }
-
+   
+   export const userAuthRegister=async(payload:any)=>{
+    const response=await axios.post(`http://localhost:9000/v1/api/register`,payload);
+    return response?.data
+   }
